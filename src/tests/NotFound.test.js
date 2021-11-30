cd17871a7f76f52973602b042b7cfebedec6e8e5
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-11-30 19:07:57 -0300

    Requisito 4 - Test content in NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from '../RenderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente <NotFound.js />', () => {
+  test('Testa se a página contém um heading h2 com "Page requested not found"', () => {
+    RenderWithRouter(<NotFound />);
+    const titleNotFound = (
+      screen.getByRole('heading', { name: 'Page requested not found Crying emoji' }));
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    RenderWithRouter(<NotFound />);
+    const imgNotFound = (
+      screen.getByAltText('Pikachu crying because the page requested was not found'));
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
