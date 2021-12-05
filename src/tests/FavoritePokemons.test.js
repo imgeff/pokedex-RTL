f6fab795fec01e3022db35923fcb5b75e9474a48
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-12-05 17:10:00 -0300

    Requisito 3 - Refatoração de código

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,23 +2,24 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import RenderWithRouter from '../RenderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+const favoritePokemons = [];
 
 describe('Testa o componente <FavoritePokemons />', () => {
   test('É exibido No favorite pokemon found, se  não existir favoritos', () => {
-    RenderWithRouter(<FavoritePokemons />);
-    const favoritesPokemons = FavoritePokemons.defaultProps.pokemons;
-    if (favoritesPokemons.length === 0) {
-      const NoFavorite = screen.getByText(/No favorite pokemon found/i);
-      expect(NoFavorite).toBeInTheDocument();
-    }
+    RenderWithRouter(<FavoritePokemons pokemons={ favoritePokemons } />);
+    const NoFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(NoFavorite).toBeInTheDocument();
   });
 
   test('Teste se é exibido todos os cards de pokémons favoritados', () => {
-    RenderWithRouter(<FavoritePokemons />);
-    const favoritesPokemons = FavoritePokemons.defaultProps.pokemons;
-    if (favoritesPokemons.length > 0) {
-      const pokemonDetail = screen.getAllByRole('link', { name: /More details/i });
-      expect(pokemonDetail.length).toBe(favoritesPokemons.length);
-    }
+    favoritePokemons.push(pokemons[0], pokemons[7]);
+    RenderWithRouter(<FavoritePokemons pokemons={ favoritePokemons } />);
+    favoritePokemons.forEach((pokemon) => {
+      const pokemonFavorite = screen.getByText(pokemon.name);
+      expect(pokemonFavorite).toBeInTheDocument();
+      expect(pokemonFavorite).toBeVisible();
+    });
   });
 });
